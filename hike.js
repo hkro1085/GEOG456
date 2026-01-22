518a46e73544c6b603baed34ac76f5db162f9367
hkro1085 <hkro@ad.unc.edu>
2026-01-22 13:05:37 -0500

    Create hike.js

---
+++ hike.js
@@ -0,0 +1,7 @@
+var hike = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": 1, "descrip": "Massie Gap Appalachain Trail Hike" }, "geometry": { "type": "Point", "coordinates": [ 36.6395638, -81.5076114] } }
+]
+}
